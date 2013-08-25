c2f97b389ee57c3b44efbf677669e5daf9a3a3ae
yongbaolinux@gmail.com <yongbaolinux@gmail.com@5a581b3f-7ff9-73e9-37f6-7e52fecc4309>
2013-08-25 11:57:11 +0000

    log

--- trunk/bluestone/text_analysis.c
+++ trunk/bluestone/text_analysis.c
@@ -2,6 +2,9 @@
 #include "../header/common.h"
 #endif
 
+extern void copy_element(void * [],int ,char *,int,int [],char * []);
+extern void check_if_key(int [],int i,char*,char * []);
+
 int main(int argv,char *argc[]){
     if(argv<2){
         PFS("the parameter error");
@@ -17,13 +20,13 @@ int main(int argv,char *argc[]){
     int ch=0;                   //当前读取的字符
     char temp[100];             //临时字符存放
     int status=0;                       //状态分四种 0为空白 1为字符 2为数字 3为其他字符
-    int status_3=0;                      //当status为3时 还细分多字符操作符和单字符操作符
+    int status__=0;                      //当status为3时 还细分多字符操作符和单字符操作符
     void *elements[1000];               //存放各元素的主字符串指针数组(token值)
     int i=0;                                            //主字符串指针数组和syn值数组的当前索引(两数组同步)
     int syn[1000];                              //syn值数组
-    char *keys[9] = {"if","else","do","while","for","for","int","double","float","short"};
+    char *keys[9] = {"if","else","do","while","for","int","double","float","short"};
                                                            //系统保留关键词数组
-    while((ch=fgetc(fp)) != "#" || (ch=fgetc(fp)) != EOF){
+    while((ch=fgetc(fp)) != '#' || (ch=fgetc(fp)) != EOF){
         if(ch==' '){
             //状态变化 临时字符串以' \0 '结尾
             if((status == 1) ||(status == 2)){
@@ -31,7 +34,7 @@ int main(int argv,char *argc[]){
                 //结尾后位置归0 将字符串拷贝到主字符串数组作永久保存
                 cur_pointer = 0;
                 //拷贝字符串 从临时到主
-                copy_elements(elements,i,temp,status);
+                copy_element(elements,i,temp,status,syn,keys);
                 i++;
             }
             status=0;
@@ -43,7 +46,7 @@ int main(int argv,char *argc[]){
                 //位置归0
                 cur_pointer = 0;
                 //拷贝字符串 从临时到主
-                copy_elements(elements,i,temp,2);
+                copy_element(elements,i,temp,2,syn,keys);
                 i++;
             }
             status=1;
@@ -53,7 +56,7 @@ int main(int argv,char *argc[]){
                 temp[cur_pointer] = '\0';
                 cur_pointer = 0;
                 //拷贝字符串 从临时到主
-                copy_elements(elements,i,temp,1);
+                copy_element(elements,i,temp,1,syn,keys);
                 i++;
             }
             status = 2;
@@ -64,45 +67,45 @@ int main(int argv,char *argc[]){
                 temp[cur_pointer] = '\0';
                 cur_pointer = 0;
                 //拷贝字符串 从临时到主
-                copy_elements(elements,i,temp,status);
+                copy_element(elements,i,temp,status,syn,keys);
                 i++;
             }
             status = 3;
-            swtich(ch){
+            switch(ch){
                 case '<':
-                    status_=24;
+                    status__=24;
                     elements[i] = "<";
                     syn[i] = 24;
                     i++;
                     break;
                 case '>':
-                    if(status_==24){
-                        //操作符匹配结束 status_置0
-                        status_ = 0;
+                    if(status__==24){
+                        //操作符匹配结束 status__置0
+                        status__ = 0;
                         elements[--i] = "<>";
                         syn[i] = 19;
                     } else {
                         elements[i] = ">";
                         syn[i] = 23;
-                        status_ = 23;
+                        status__ = 23;
                     }
                     i++;
                     break;
                 case '=':
-                    if(status_ == 22){
-                        status_ = 0;
+                    if(status__ == 22){
+                        status__ = 0;
                         elements[--i] = "==";
                         syn[i] = 18;
-                    } else if(status_ == 23){
-                        status_ = 0;
+                    } else if(status__ == 23){
+                        status__ = 0;
                         elements[--i] = ">=";
                         syn[i] = 21;
-                    }else if(status_ == 24){
-                        status_ = 0;
+                    }else if(status__ == 24){
+                        status__ = 0;
                         elements[--i] = "<=";
                         syn[i] = 20;
                     } else {
-                        status_ = 22;
+                        status__ = 22;
                         syn[i] = 22;
                     }
                     i++;
@@ -120,7 +123,7 @@ int main(int argv,char *argc[]){
                     syn[i] = 14;
                     break;
                 case '/':
-                    elements[i] = '/';
+                    elements[i] = "/";
                     syn[i] = 15;
                     break;
                 case '?':
@@ -141,21 +144,21 @@ int main(int argv,char *argc[]){
 }
 
 //copy elements and analysis the element and write the syn array
-void copy_element(void * elements,int i,char temp,int status){
+void copy_element(void * elements[],int i,char *temp,int status,int syn[],char *keys[]){
     if(status == 1){
         elements[i] = (char*)calloc(1,strlen(temp)+1);
         strcpy(elements[i],temp);
-        check_if_key(syn,i,temp);
+        check_if_key(syn,i,temp,keys);
     } else if(status == 2){
         int element = atoi(temp);
-        elements[i] = (int*)calloc(1,sizeof(int));
-        *elements[i] = element;
+        elements[i] = calloc(1,sizeof(int));
+        *(int*)elements[i] = element;
         syn[i] = 11;
     }
 }
 
 //检查字符是否为系统保留关键词
-void check_if_key(int syn[],int i,temp){
+void check_if_key(int syn[],int i,char* temp,char *keys[]){
     int j=0;
     for(j=0;j<9;j++){
         if(strcmp(temp,keys[j])==0){

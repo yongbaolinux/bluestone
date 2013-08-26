98f91a3183244badd5b4de9bcc95158f111b96dd
yongbaolinux <yongbaolinux@gmail.com>
2013-08-26 02:33:04 +0000

    log

---
+++ bluestone/header/common.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define PFD(x)     printf("%d",x);\
+                                printf("%s","\n");
+#define PFC(x)      printf("%c",x);\
+                                printf("%s","\n");
+#define PFS(x)       printf("%s",x);\
+                                printf("%s","\n");
+

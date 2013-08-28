4c6b8067db016f53e4c787b44455234779f85488
yongbaolinux@gmail.com <yongbaolinux@gmail.com@5a581b3f-7ff9-73e9-37f6-7e52fecc4309>
2013-08-28 04:52:40 +0000

    log

--- trunk/bluestone/header/common.h
+++ trunk/bluestone/header/common.h
@@ -1,11 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<time.h>
+#include<errno.h>
 
 #define PFD(x)     printf("%d",x);\
                                 printf("%s","\n");
 #define PFC(x)      printf("%c",x);\
                                 printf("%s","\n");
 #define PFS(x)       printf("%s",x);\
+                                printf("%s","\n");
+#define PFX(x)       printf("%0x",x);\
                                 printf("%s","\n");
 

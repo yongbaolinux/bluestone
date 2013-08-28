52f2b605c2c3d4ba8d3d0fa98efa7ce4fcb9ed3f
yongbaolinux <yongbaolinux@gmail.com>
2013-08-28 08:01:23 +0000

    log

--- bluestone/ls.c
+++ bluestone/ls.c
@@ -1,4 +1,5 @@
 #include<sys/stat.h>
+#include<dirent.h>
 #include "./header/common.h"
 
 typedef struct _stat {
@@ -6,30 +7,45 @@ typedef struct _stat {
     char *fileName;
 } fileInfo;
 
-char* perm[8] ={"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};
+char* type[7] ={"p","c","d","b","-","l","s"};                                                   //文件类型索引表
+char* perm[8] ={"---","--x","-w-","-wx","r--","r-x","rw-","rwx"};                //文件访问权限索引表
 
 int main(int argv,char* argc[]){
     if(argv <  2){
-        fprintf(stderr,"usage:%s <filepathname>",argc[0]);
-        exit(-1);
+//        fprintf(stderr,"usage:%s <filepathname>",argc[0]);
+//        exit(-1);
+        argv=2;
+        argc[1] = ".";
     }
     int i=0;
-    fileInfo * fileInfos = (fileInfo*)calloc(argv,sizeof(fileInfo));
-    memset(fileInfos,0,sizeof(fileInfo)*argv);
-    for(i=1;i<argv;i++){
-        if(stat(argc[i],&(fileInfos[i-1].fileStat))<0){
+    fileInfo * fileInfos = (fileInfo*)calloc(1,sizeof(fileInfo));
+    fileInfos->fileName = (char*)calloc(strlen(argc[1])+1,sizeof(char));
+    
+        if(stat(argc[1],&(fileInfos->fileStat))<0){
             fprintf(stderr,"error:%s",strerror(errno));
-            continue;
+            exit(-1);
         }
-//        if(S_ISREG(fileInfos[i-1].fileStat.st_mode)){
-//            
-//        }
-        fileInfos[i-1].fileName = (char*)calloc(strlen(argc[i])+1,sizeof(char));
-        strcpy(fileInfos[i-1].fileName,argc[i]);
-        printf("%s",perm[fileInfos[i-1].fileStat.st_mode >>6 & 7]);                    //文件拥有者的权限
-        printf("%s",perm[fileInfos[i-1].fileStat.st_mode >>3 & 7]);                    //文件拥有者的所在组成员的权限
-        printf("%s",perm[fileInfos[i-1].fileStat.st_mode & 7]);                           //其他成员的权限
-        PFS(ctime(&fileInfos[i-1].fileStat.st_ctim.tv_sec));
+
+    if(!S_ISDIR(fileInfos->fileStat.st_mode)){
+            strcpy(fileInfos->fileName,argc[1]);
+            //文件类型
+            printf("%s",type[(fileInfos->fileStat.st_mode>>12 & 15)/2]);
+            //文件权限
+            printf("%s",perm[fileInfos->fileStat.st_mode >>6 & 7]);                    //文件拥有者的权限
+            printf("%s",perm[fileInfos->fileStat.st_mode >>3 & 7]);                    //文件拥有者的所在组成员的权限
+            printf("%s",perm[fileInfos->fileStat.st_mode & 7]);                           //其他成员的权限
+            printf("%s"," ");
+            //最后被修改的时间
+            printf("%s",ctime(&fileInfos->fileStat.st_mtim.tv_sec));
+            printf("%s"," ");
+            //文件大小
+            printf("%d",fileInfos->fileStat.st_size);
+    } else if(S_ISDIR(fileInfos->fileStat.st_mode)){
+            if()    
     }
+}
+
+char* basename(){
+    
 }
 
